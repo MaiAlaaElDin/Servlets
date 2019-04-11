66b1b5591ad5a9bafd3dffe69299877be43b8c5f
MaiAlaa <mai.alaa.elsaeed@gmail.com>
2019-04-11 21:57:07 +0200

    First Web Servlet
    Web Servlet Demo

---
+++ MyFirstWebApp/src/java/ServletPackage/MyServlet.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServletPackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ * @author May
+ */
+public class MyServlet implements Servlet{
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        PrintWriter out = res.getWriter();
+        out.println("Welcome to My First Web App!");
+    }
+
+    @Override
+    public String getServletInfo() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
